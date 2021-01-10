99c21e73884540cabdbe3cede759b91e66b32ce3
Madusha <madusha.pallawela@bbc.co.uk>
2021-01-10 14:08:18 +0000

    Refactored hardcoded images

--- RB+/app.js
+++ RB+/app.js
@@ -1,20 +1,33 @@
+window.onload = function () {
+    let images = ['image1', 'image2', 'image3']
+    let imageHtml = images.map((image, index) => {
+        let selected = index == 0 ? "selected" : ""
+
+        return `<div class="carouselItem ${selected}">
+                  <img src="images/${image}.jpg">
+               </div>`
+    }).join('/n')
+
+    document.getElementById('carouselRow').innerHTML = imageHtml
+};
 
 let currentIndex = 0
 let previousIndex = 0
+
 function slide(direction) {
+
     let currentOffset = parseInt(document.getElementById('carouselRow').style.left)
     let offset = 80
     previousIndex = currentIndex
 
     if (direction == "right") {
         currentIndex++
+        currentOffset -= offset
     }
     else {
-        offset = -offset
         currentIndex--
+        currentOffset += offset
     }
-
-    currentOffset -= offset
 
     if (currentIndex < 0) {
         currentIndex = 0
